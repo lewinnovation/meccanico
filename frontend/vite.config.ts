f461c0997cb190450ec85a2ffbd7e5fdd66a55ec
Vidur Butalia <vidurbutalia@gmail.com>
2025-12-23 13:35:36 +0530

    fix: pass hostname to vite dev server

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +11,7 @@ export default defineConfig({
   },
   server: {
     port: process.env.PORT ?? 3000,
+    allowedHosts: process.env.ALLOWED_HOSTS?.split(",") ?? [],
     proxy: {
       "/api": {
         target: "http://localhost:4000",
